aa585310a04907e10d92feb01391f47bf2447a32
akbarivankaa <akbarivankaa@gmail.com>
2026-03-08 12:33:42 +0700

    (bismillah_amin)

---
+++ GUIDED/main.js
@@ -0,0 +1,27 @@
+// function ganjilGenap(input) {
+//    if (input % 2 === 0) {
+//       return true;
+//   }
+// }
+
+// function absolutValue (input) {
+//    if (input < 0) {
+//        return -input;
+//   }
+// }
+
+// let apakahGenap = absolutValue(-145);
+// console.log(apakahGenap);
+
+// let nama = "Ivan" ;
+// console.log('hallo, nama saya ${nama}');
+
+
+object
+const user = {
+    "first name" : "Akbar",
+    "last name" : "Ivanka",
+    age: 21
+};
+
+console.log(user["first name"]);
